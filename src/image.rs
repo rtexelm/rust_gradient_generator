d18fffc1fdf8e509773cadb79195089f3d628fc5
rtexelm <texelist@gmail.com>
2023-06-28 16:14:58 -0400

    Add rotate function

--- src/image.rs
+++ src/image.rs
@@ -10,11 +10,44 @@ pub struct Image {
 
 impl Image {
     pub fn new(width: u32, height: u32) -> Self {
-        let pixels: Vec<Vec<Rgb<u8>>> = vec![vec![Rgb([0,0,0]); width as usize] height as usize];
+        let pixels: Vec<Vec<Rgb<u8>>> = vec![vec![Rgb([0, 0, 0]); width as usize]; height as usize];
         Image {
             width,
             height,
             pixels,
+        }
+    }
+
+    pub fn new_from_file(path: &str) -> Result<Self, image::ImageError> {
+        let img = image::open(path)?.to_rgb8();
+
+        let width = img.width();
+        let height = img.height();
+
+        let mut pixels = vec![vec![Rgb([0, 0, 0]); width as usize]; height as usize];
+
+        for (x, y, pixel) in img.enumerate_pixels() {
+            pixels[y as usize][x as usize] = *pixel;
+        }
+
+        Ok(Image {
+            width,
+            height,
+            pixels,
+        })
+    }
+
+    pub fn generate_gradient_image(&mut self, color1: [u8; 3], color2: [u8; 3]) {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let r = ((color2[0] as f32 - color1[0] as f32) * (x as f32 / self.width as f32)
+                    + color1[0] as f32) as u8;
+                let g = ((color2[1] as f32 - color1[1] as f32) * (y as f32 / self.height as f32)
+                    + color1[1] as f32) as u8;
+                let b = ((color2[2] as f32 - color1[2] as f32) * (x as f32 + y as f32)
+                    / (self.height as f32 + self.height as f32)
+                    + color1[2] as f32) as u8;
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
 #![allow(unused_variables)]
+// mod filters;
+mod image;
 fn main() {
     println!("Hello, world!");
 }
